aacbe71a0191b26b441ecb1fdc98d315d2b2bcdd
MattGraffunder <MattGraffunder@users.noreply.github.com>
2016-09-30 15:53:53 -0500

    Initial Commit

---
+++ src/ServiceStackTest/ServiceStackTest.ServiceInterface/HelloService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using ServiceStackTest.ServiceModel;
+
+namespace ServiceStackTest.ServiceInterface
+{
+    public class HelloService : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
---
+++ src/ServiceStackTest/ServiceStackTest.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using ServiceStackTest.ServiceInterface;
+using ServiceStackTest.ServiceModel;
+using ServiceStack.Testing;
+using ServiceStack;
+
+namespace ServiceStackTest.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private readonly ServiceStackHost appHost;
+
+        public UnitTests()
+        {
+            appHost = new BasicAppHost(typeof(HelloService).Assembly)
+            {
+                ConfigureContainer = container =>
+                {
+                    //Add your IoC dependencies here
+                }
+            }
+            .Init();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var service = appHost.Container.Resolve<HelloService>();
+
+            var response = (HelloResponse)service.Any(new Hello { Name = "World" });
+
+            Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+        }
+    }
+}
